b503420de12dc5edb1177b24bc4e7f9736a93e58
Carlos Segura Garcia <lithos.contact@gmail.com>
2024-02-13 15:45:01 +0100

    refactor unit tests

--- src/lib/components/BaseInput.spec.ts
+++ src/lib/components/BaseInput.spec.ts
@@ -1,11 +1,11 @@
 import { render } from '@testing-library/vue';
 import BaseInput from './BaseInput.vue';
 
-const renderComponent = (props = {}) => render(BaseInput, { props });
+const renderComponent = ({ props = {} }) => render(BaseInput, { props });
 
 describe('Given a BaseInput component', () => {
-	test('Then it displays label when passed as a prop', async () => {
-		const { getByText } = renderComponent({ label: 'Test label' });
+	test('Then it displays label when passed as a prop', () => {
+		const { getByText } = renderComponent({ props: { label: 'Test label' } });
 		expect(getByText('Test label')).toBeTruthy();
 	});
 });
--- src/lib/components/BaseTextarea.spec.ts
+++ src/lib/components/BaseTextarea.spec.ts
@@ -1,11 +1,11 @@
 import { render } from '@testing-library/vue';
 import BaseTextarea from './BaseTextarea.vue';
 
-const renderComponent = (props = {}) => render(BaseTextarea, { props });
+const renderComponent = ({ props = {} }) => render(BaseTextarea, { props });
 
 describe('Given a BaseTextarea component', () => {
 	test('Then it displays label when passed as a prop', async () => {
-		const { getByText } = renderComponent({ label: 'Test label' });
+		const { getByText } = renderComponent({ props: { label: 'Test label' } });
 		expect(getByText('Test label')).toBeTruthy();
 	});
 });
