a92a0d18b8818be054617c0936360ab440ff2243
Carlos Segura Garcia <lithos.contact@gmail.com>
2024-02-14 16:17:33 +0100

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue(), cssInjectedByJsPlugin()],
+	plugins: [vue(), cssInjectedByJsPlugin(), dts()],
 	build: {
 		sourcemap: true,
 		copyPublicDir: false,
